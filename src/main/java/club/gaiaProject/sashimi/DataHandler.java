86c4c7aa80f191c14e936df3ca9ed4da17d90010
ly <aa@qq.com>
2018-05-06 22:57:37 +0800

    add start date , end date and so on

--- src/main/java/club/gaiaProject/sashimi/DataHandler.java
+++ src/main/java/club/gaiaProject/sashimi/DataHandler.java
@@ -3,15 +3,7 @@ package club.gaiaProject.sashimi;
 import club.gaiaProject.sashimi.util.ExcelUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import club.gaiaProject.sashimi.bean.AlarmBean;
 import club.gaiaProject.sashimi.bean.DeviceBean;
@@ -22,51 +14,81 @@ import club.gaiaProject.sashimi.bean.EventBean;
  */
 public class DataHandler {
     private List<EventBean> events;//execl 中的全部记录
-    private Calendar calendar = Calendar.getInstance();
+    private Long startTime;
+    private Long endTime;
+    private Integer dayCount;
     //采集信息
     private List<EventBean> overhaulEvents = new ArrayList<EventBean>();//（23 - 5）点的检修记录
+    private Calendar calendar = Calendar.getInstance();
     private Map<String, DeviceBean> deviceMapping = new HashMap<String, DeviceBean>();//设备id -> 设备信息
-    private Map<String, List<AlarmBean>> deviceErrorNum = new HashMap<String, List<AlarmBean>>();//设备ID -> 告警信息
+    private Map<String, List<AlarmBean>> deviceErrorNum = new HashMap<>();//设备ID -> 告警信息
     private List<List<EventBean>> doubtfulOverhulEvents = new ArrayList<List<EventBean>>();//疑似检修
     private Integer count = 0;//有效报警
     //临时记录
     private Map<String, Long> subwayErrorTime = new HashMap<String, Long>();//每个站最后一次报警时间
     private Map<String, List<EventBean>> subwayErrorEvent = new HashMap<String, List<EventBean>>();//
     private Set<Integer> doubtfulEventsId = new HashSet<Integer>();//疑似的ID
-    //一个车站5分种内 报警 超过5次 判定为 疑似检修
+    //一个车站10分种内 报警 超过5次 判定为 疑似检修
     private static Long LIMIT_ERROR_TIME = 1000 * 60 * 10L;//最小报警间隔
-    private static Integer LIMIT_ERROR_NUM = 5;//最小报警数
+    private Integer LIMIT_ERROR_NUM;//最小报警数
 
     public DataHandler(List<EventBean> events) {
         this.events = events;
     }
 
     public void print() {
-        System.out.println("疑似检修次数："+doubtfulEventsId.size());
+        System.out.println("数据开始时间：" + ExcelUtils.DATEFORMAT.format(new Date(startTime)));
+        System.out.println("数据结束时间：" + ExcelUtils.DATEFORMAT.format(new Date(endTime)));
+        System.out.println("数据跨度：" + dayCount + "天");
+        System.out.println("疑似检修次数：" + doubtfulEventsId.size());
         System.out.println("有效报警数" + count);
 
-        System.out.println("午夜检修告警："+overhaulEvents.size());
-        System.out.println("---各站有效报警数---");
+        System.out.println("午夜检修告警：" + overhaulEvents.size());
+        System.out.println("---有效报警数---");
         for (Map.Entry<String, List<AlarmBean>> entry : deviceErrorNum.entrySet()) {
-            System.out.println(deviceMapping.get(entry.getKey()).getName() + "  " + entry.getValue().size());
+            System.out.println(deviceMapping.get(entry.getKey()).getSubway() + "  " + deviceMapping.get(entry.getKey()).getName() + "  " + entry.getValue().size());
         }
-        for(List<EventBean> eventBeans:doubtfulOverhulEvents){
+        System.out.println("————疑似检修数据————");
+        for (List<EventBean> eventBeans : doubtfulOverhulEvents) {
 
-            for(EventBean e : eventBeans){
-                System.out.print(ExcelUtils.DATEFORMAT.format(new Date(e.getTimeStamp())) +" ");
-                System.out.print(e.getDevice().getName() +" ");
-                System.out.print(e.getDevice().getSubway()+" ");
-                System.out.print(e.getDevice().getId()+" ");
+            for (EventBean e : eventBeans) {
+                System.out.print(ExcelUtils.DATEFORMAT.format(new Date(e.getTimeStamp())) + " ");
+                System.out.print(e.getDevice().getName() + " ");
+                System.out.print(e.getDevice().getSubway() + " ");
+                System.out.print(e.getDevice().getId() + " ");
                 System.out.print(e.getAlarm().getInfo());
                 System.out.println();
             }
             System.out.println("---------");
+        }
+        System.out.println("————作业产生告警数据————");
+        for (EventBean e : overhaulEvents) {
+
+            System.out.print(ExcelUtils.DATEFORMAT.format(new Date(e.getTimeStamp())) + " ");
+            System.out.print(e.getDevice().getName() + " ");
+            System.out.print(e.getDevice().getSubway() + " ");
+            System.out.print(e.getDevice().getId() + " ");
+            System.out.print(e.getAlarm().getInfo());
+            System.out.println();
         }
 
 
     }
 
     public void handle() {
+        EventBean startEventBean = events.get(0);
+        EventBean endEventBean = events.get(events.size() - 1);
+        startTime = startEventBean.getTimeStamp();
+        endTime = endEventBean.getTimeStamp();
+        Long dayStamp = (endTime - startTime) / (1000 * 60 * 60 * 24L);
+        dayCount = dayStamp.intValue() + 1;//天数
+        if (dayCount >= 30) {
+            LIMIT_ERROR_NUM = 5;
+            System.out.println("阈值："+5);
+        } else {
+            LIMIT_ERROR_NUM = 3;
+            System.out.println("阈值："+3);
+        }
         Iterator<EventBean> it = events.iterator();
         //第一次先把疑似检修和确认的检修剔除，生成 设备基本信息
         while (it.hasNext()) {
@@ -74,6 +96,7 @@ public class DataHandler {
             Date time = new Date(event.getTimeStamp());
             calendar.setTime(time);
 
+            //验证此条告警是否是 午夜检修
             if (checkDeviceOverhaul(event)) {
                 it.remove();
                 continue;
@@ -98,6 +121,10 @@ public class DataHandler {
         //处理可确信的报告
         for (EventBean event : events) {
             if (!doubtfulEventsId.contains(event.getId())) {//非疑似报警
+                //
+
+
+                //各个设备有多少次
                 if (deviceErrorNum.containsKey(event.getDevice().getId())) {
                     deviceErrorNum.get(event.getDevice().getId()).add(event.getAlarm());
                 } else {
@@ -105,6 +132,8 @@ public class DataHandler {
                     alarms.add(event.getAlarm());
                     deviceErrorNum.put(event.getDevice().getId(), alarms);
                 }
+
+                //有效告警的总数
                 this.count++;
             }
         }
@@ -142,10 +171,10 @@ public class DataHandler {
     }
 
 
-    //0-4 23以后 设备检修
+    //0-4 23以后 设备检修 判定为 午夜检修
     private Boolean checkDeviceOverhaul(EventBean eventBean) {
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        if (hour < 5 || hour >= 23) {
+        if (hour < 4 || hour >= 23) {
             overhaulEvents.add(eventBean);
             return true;
         }
